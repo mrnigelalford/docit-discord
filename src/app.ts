50d7b39260a8d301e76e839ff6a54a10b9915446
atlmapper <ngalford@gmail.com>
2023-08-12 12:28:47 -0400

    wip

--- src/app.ts
+++ src/app.ts
@@ -18,30 +18,30 @@ const removeID = (inputString: string) => inputString.replace(/<@\d+>/g, '').tri
 client.on("messageCreate", async (message) => {
   const { guildId, content, author } = message;
 
-  if(!content.length || message.author.bot || message.author.id === '1138959598536097842') return
+  if (!content.length || message.author.bot || message.author.id === '1138959598536097842') return
   switch (guildId) {
     case '899867212309987378':
       const response = await getChat(removeID(content), 'balancer');
       // send message to user in thread
       setMessageToThreads(message, response)
-      //TODO: respond to user with follow up questions here.
+    //TODO: respond to user with follow up questions here.
   }
 });
 
 const setMessageToThreads = async (message: Message, response: any) => {
-    const threadChannel = await message.startThread({
-      name: 'docIT',
-      autoArchiveDuration: 60,
-      type: ChannelType.PrivateThread,
-      message: { content: response.answer },
-      reason: 'separate thread for documentation',
-    });
-    threadChannel.members.add(message.author);
-    threadChannel.send(response.answer);
+  const threadChannel = await message.startThread({
+    name: 'docIT',
+    autoArchiveDuration: 60,
+    type: ChannelType.PrivateThread,
+    message: { content: response.answer },
+    reason: 'separate thread for documentation',
+  });
+  threadChannel.members.add(message.author);
+  threadChannel.send(response.answer);
 }
 
 const getChat = async (question: string, projectID: string): Promise<JSON | unknown> => {
-  const getResponse = await axios.get(`http://localhost:3000/questions`, {data: { question, projectID }});
+  const getResponse = await axios.get(`${process.env.DOCIT_INFRA}/questions`, { data: { question, projectID }, auth: { username: 'admin', password: process.env.DOCIT_AUTH } });
 
   return getResponse.data;
 }
