0ffe1fae5f0a4f26350107915828634456eba48d
atlmapper <ngalford@gmail.com>
2023-08-12 17:43:31 -0400

    adding logging

--- src/textTool.ts
+++ src/textTool.ts
@@ -3,6 +3,7 @@ const tokenizer = new natural.SentenceTokenizer();
 
 const splitIntoMarkdownParagraphs = (text) => {
   // Check if the input is a string
+  console.log('text: ', text);
   if (typeof text !== 'string') {
     throw new TypeError('The provided input is not a string.');
   }
