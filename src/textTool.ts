1b040641d68ea7339ccb67f529aad8eb92258ee3
atlmapper <ngalford@gmail.com>
2023-08-12 16:51:11 -0400

    added text filter

--- src/textTool.ts
+++ src/textTool.ts
@@ -2,18 +2,23 @@ import natural from 'natural';
 const tokenizer = new natural.SentenceTokenizer();
 
 const splitIntoMarkdownParagraphs = (text) => {
+  // Check if the input is a string
+  if (typeof text !== 'string') {
+    throw new TypeError('The provided input is not a string.');
+  }
+
   const sentences = tokenizer.tokenize(text);
   const paragraphs = [];
   let currentParagraph = [];
 
   sentences.forEach((sentence, idx) => {
-      currentParagraph.push(sentence);
+    currentParagraph.push(sentence);
 
-      // Check for logical breaks or if we've reached the end of the text.
-      if (sentence.endsWith('.') || idx === sentences.length - 1) {
-          paragraphs.push(currentParagraph.join(' '));
-          currentParagraph = [];
-      }
+    // Check for logical breaks or if we've reached the end of the text.
+    if (sentence.endsWith('.') || idx === sentences.length - 1) {
+      paragraphs.push(currentParagraph.join(' '));
+      currentParagraph = [];
+    }
   });
 
   return paragraphs.filter(paragraph => paragraph.trim().length > 0).join('\n\n');
