79b9f71a2bdfd16c8c923b49785cea6f968ac4dd
atlmapper <ngalford@gmail.com>
2023-08-13 01:14:13 -0400

    adding thread support to allow convos to continue in same thread

--- src/app.ts
+++ src/app.ts
@@ -6,43 +6,47 @@ import 'dotenv/config';
 const client = new Client({ intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMessages, IntentsBitField.Flags.DirectMessages] });
 const removeID = (inputString: string) => inputString.replace(/<@\d+>/g, '').trim();
 
-const setMessageToThreads = async (message: Message, response: any) => {
-  const threadChannel = await message.startThread({
-    name: 'docIT',
-    autoArchiveDuration: 60,
-    type: ChannelType.PrivateThread,
-    message: { content: response.answer },
-    reason: 'separate thread for documentation',
-  });
-  threadChannel.members.add(message.author);
-  threadChannel.send(response.answer);
-}
-
-client.once('ready', (c) =>	console.log(`Ready! Logged in as ${c.user.tag}`));
-
-/*
-client.on('interactionCreate', interaction => {
-  console.log(interaction);
-});
-*/
-
-client.on("messageCreate", async (message) => {
-  const { guildId, content, author } = message;
-  if (!content.length || author.bot || author.id === process.env.DISCORD_BOT_ID) return
-  switch (guildId) {
-    case '899867212309987378': // TODO: Move this to a db function to return valid variables here
-      const response = await getChat(removeID(content), 'balancer');
-      // send message to user in thread
-      setMessageToThreads(message, response)
-    //TODO: respond to user with follow up questions here.
-  }
-});
-
 const getChat = async (question: string, projectID: string): Promise<JSON | unknown> => {
   const getResponse = await axios.get(`${process.env.DOCIT_INFRA}/questions`, { data: { question, projectID }, auth: { username: 'admin', password: process.env.DOCIT_AUTH } });
 
   return getResponse.data;
 }
+
+client.once('ready', () => {
+  console.log(`Ready! Logged in as ${client.user.tag}`);
+});
+
+client.on('messageCreate', async (message) => {
+  const { guildId, content, author } = message;
+  if (!content.length || author.bot || author.id === process.env.DISCORD_BOT_ID) return;
+
+  const guild = client.guilds.cache.get(guildId);
+  if (!guild) return;
+
+  switch (guildId) {
+    case '899867212309987378': // TODO: Move this to a db function to return valid variables here
+      const response = await getChat(removeID(content), 'balancer');
+
+      // Check if the message is in a thread
+      if (message.thread) {
+        // Send response in the same thread
+        message.thread.send(response.answer);
+      } else {
+        // Send response in a new thread
+        const threadChannel = await message.startThread({
+          name: 'Response Thread',
+          autoArchiveDuration: 1440, // Set the auto-archive duration as needed
+          type: ChannelType.PrivateThread,
+          startMessage: response.answer,
+          reason: 'separate thread for documentation',
+        });
+
+        threadChannel.members.add(message.author);
+        threadChannel.send(response.answer);
+      }
+      break;
+  }
+});
 
 client.login(process.env.DISCORD_TOKEN);
 
