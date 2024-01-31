4ad7f0e02f40781d7bd43ae47362f15e6850ad32
atlmapper <ngalford@gmail.com>
2024-01-30 19:04:54 -0500

    bot listens to all channel messages, will respond if it can otherwise will not

--- src/app.ts
+++ src/app.ts
@@ -1,65 +1,85 @@
 // @ts-nocheck
 import axios from 'axios';
-import { ChannelType, Client, IntentsBitField, Message } from 'discord.js';
+import { ChannelType, Client, IntentsBitField } from 'discord.js';
 import 'dotenv/config';
 import { callGetSecret } from './secrets';
 
-export const client = new Client({ intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMessages, IntentsBitField.Flags.DirectMessages] });
-const removeID = (inputString: string) => inputString.replace(/<@\d+>/g, '').trim();
+export const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.DirectMessages,
+    IntentsBitField.Flags.MessageContent
+  ]
+});
 
+const removeID = (inputString: string) => inputString.replace(/<@\d+>/g, '').trim();
 
 const getChat = async (question: string, projectID: string): Promise<JSON | unknown> => {
   const DOCIT_AUTH = await callGetSecret('DOCIT_AUTH');
   const DOCIT_INFRA = await callGetSecret('DOCIT_INFRA');
-  const getResponse = await axios.post(`${DOCIT_INFRA}/questions`,
+  const response = await axios.post(`${DOCIT_INFRA}/questions`,
     { question, projectID },
     {
       headers: { 'Content-Type': 'application/json' },
       auth: { username: 'admin', password: DOCIT_AUTH }
     }
   );
-  return getResponse.data;
-}
+  return response.data;
+};
 
 client.once('ready', () => {
   console.log(`Ready! Logged in as ${client.user.tag}`);
 });
 
 client.on('messageCreate', async (message) => {
-  const { guildId, content, author } = message;
+  const { content, author } = message;
   const DISCORD_BOT_ID = await callGetSecret('DISCORD_BOT_ID');
+
   if (!content.length || author.bot || author.id === DISCORD_BOT_ID) return;
 
-  const guild = client.guilds.cache.get(guildId);
-  if (!guild) return;
+  let modifiedContent = content;
 
-  switch (guildId) {
-    case '899867212309987378': // TODO: Move this to a db function to return valid variables here
-      const response = await getChat(removeID(content), 'sui');
+  // Check if the content string does not contain 'in SUI', and append it if necessary
+  if (!content.includes('in SUI')) {
+    modifiedContent = `${content} in SUI`;
+  }
 
-      const clean = JSON.parse(response.answer).choices[0].message.content.replace(/\n/g, " ")
+  const response = await getChat(removeID(modifiedContent), 'sui');
+  if (!response || typeof response.answer !== 'string') return;
 
-      // Check if the message is in a thread
-      if (message.position) {
-        message.reply(clean);
-      } else {
-        // Send response in a new thread
-        const threadChannel = await message.startThread({
-          name: 'Response Thread',
-          autoArchiveDuration: 1440, // Set the auto-archive duration as needed
-          type: ChannelType.PrivateThread,
-          startMessage: clean,
-          reason: 'separate thread for documentation',
-        });
+  const getAnswer = JSON.parse(response.answer).choices[0].message.content.replace(/\n/g, " ");
 
-        threadChannel.members.add(message.author);
-        threadChannel.send(clean);
-      }
-      break;
+  // Check for direct mention and unknown answer
+  if (message.mentions.members.find(m => m.id === DISCORD_BOT_ID) && getAnswer.trim() === "Sorry, I don't know how to help with that.") {
+    message.reply(getAnswer);
+    return;
+  } else if (getAnswer.trim() === "Sorry, I don't know how to help with that.") {
+    return; // Do not send back a response
+  }
+
+  // Check if the message is part of a thread
+  const isThreadMessage = message.channel.type === ChannelType.PublicThread || message.channel.type === ChannelType.PrivateThread;
+
+  if (isThreadMessage) {
+    // Reply in the current thread
+    message.reply(getAnswer);
+  } else {
+    // Send response in a new thread
+    const threadChannel = await message.startThread({
+      name: 'Response Thread',
+      autoArchiveDuration: 1440,
+      type: ChannelType.PrivateThread,
+      startMessage: getAnswer,
+      reason: 'separate thread for documentation',
+    });
+
+    threadChannel.members.add(message.author);
+    threadChannel.send(getAnswer);
   }
 });
 
 (async () => {
   const DISCORD_TOKEN = await callGetSecret('DISCORD_TOKEN');
-  client.login(DISCORD_TOKEN)}
-)();
+  client.login(DISCORD_TOKEN);
+})();
